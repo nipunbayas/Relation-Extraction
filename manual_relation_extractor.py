ca10f4ecf05cdc2cfbae0b914be5fbe86e66af0b
Nipun Bayas <nsbayas@gmail.com>
2017-04-06 15:19:27 -0400

    Removed liblinear library. Using sklearn LinearSVC instead

--- manual_relation_extractor.py
+++ manual_relation_extractor.py
@@ -7,11 +7,18 @@
 # 5) studied at
 
 import re
+import pandas as pd
+
+from sklearn.datasets import load_files
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.svm import LinearSVC
 
 if __name__ == "__main__":
-    input_file = 'sample_test.tsv'
+    input_file = 'test.tsv'
     output_file = 'output_manual_extractor.tsv'
     output = open(output_file, 'w')
+
+    test_model_file = load_files(input_file, encoding='latin-1')
 
     with open(input_file) as inputted_file:
         lines = inputted_file.read().splitlines()  # read input file
@@ -30,3 +37,8 @@ if __name__ == "__main__":
         else:
             output.write("%s\t%s\t%s\t%s\t%s\n" % (sentences[0], sentences[1], sentences[2], sentences[3], 'no'))
     output.close()
+
+    test_generated_file = load_files(output_file, encoding='latin-1')
+
+    vectors = CountVectorizer.fit_transform(test_model_file.data)
+    print vectors.shape
