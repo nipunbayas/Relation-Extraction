c6e5614912778a4b1ba148caf6a5ac886b61ed61
Nipun Bayas <nsbayas@gmail.com>
2017-04-06 02:21:00 -0400

    Added regex patterns for the manual_relation_extractor and the output file on a sample dataset

--- manual_relation_extractor.py
+++ manual_relation_extractor.py
@@ -8,11 +8,25 @@
 
 import re
 
-if __name__  == "__main__":
-    strs = "Wassup? Are you okay? Not Ok"
+if __name__ == "__main__":
+    input_file = 'sample_test.tsv'
+    output_file = 'output_manual_extractor.tsv'
+    output = open(output_file, 'w')
 
-    match = re.search(r'\bNot Ok\b', strs)
-    if match:
-        print "Found"
-    else:
-        print "Not Found"
+    with open(input_file) as inputted_file:
+        lines = inputted_file.read().splitlines()  # read input file
+
+    for line in lines:
+        sentences = line.split('\t')
+        sentence = sentences[2]
+
+        match = re.search(r'\beducated at\b', sentence, flags=re.IGNORECASE) or \
+            re.search(r'\bgraduated from\b', sentence, flags=re.IGNORECASE) or \
+            re.search(r'\bmatriculated at\b', sentence, flags=re.IGNORECASE) or \
+            re.search(r'\battended\b', sentence, flags=re.IGNORECASE) or \
+            re.search(r'\bstudied at\b', sentence, flags=re.IGNORECASE)
+        if match:
+            output.write("%s\t%s\t%s\t%s\t%s\n" % (sentences[0], sentences[1], sentences[2], sentences[3], 'yes'))
+        else:
+            output.write("%s\t%s\t%s\t%s\t%s\n" % (sentences[0], sentences[1], sentences[2], sentences[3], 'no'))
+    output.close()
